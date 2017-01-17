932763021abe2a51d52a25a58c0a57c5d697af9a
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-01-17 23:45:27 +0530

    Few if then

--- launch.sh
+++ launch.sh
@@ -2,10 +2,11 @@
 service=go-MyVIT
 
 #check if the service is not running
-if (( $(ps -ef | grep -v grep | grep $service | wc -l) < 1 )) then
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) < 1 ))
+then
 
 	#initiate the script again
-	cd go/src/$service
+	cd ~/go/src/$service
 	nohup ./$service &
 
 fi
